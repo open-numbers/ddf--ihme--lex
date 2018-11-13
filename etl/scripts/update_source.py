e458d1e00eb9e019370deec73fb617f7dd301f81
semio <prairy.long@gmail.com>
2018-11-13 14:42:50 +0800

    update script for new version of GBD

--- etl/scripts/update_source.py
+++ etl/scripts/update_source.py
@@ -2,6 +2,7 @@
 
 
 import os
+import tempfile
 import shutil
 
 from ddf_utils.factory import ihme
@@ -10,17 +11,30 @@ from ddf_utils.factory import ihme
 source_dir = '../source'
 
 
+def remove_old_source():
+    for f in os.listdir(source_dir):
+        if f.startswith('IHME') and f.endswith('zip'):
+            os.remove(os.path.join(source_dir, f))
+
+
 def main():
     md = ihme.load_metadata()
-    latest = md['version'].sort_values(by='version_id').iloc[-1, 0]
+    latest = md['version'].sort_values(by='id').iloc[-1, 0]
     print('latest version is: {}'.format(latest))
 
-    ihme.bulk_download(source_dir, latest, 'le')
-    for f in os.listdir(source_dir):
-        # FIXME: deal with multiple data
+    tmp_dir = tempfile.mkdtemp()
+    folders = ihme.bulk_download(tmp_dir, latest, 'le')
+    if not folders:
+        print('download was not completed.')
+        raise ValueError('downloader failed')
+
+    remove_old_source()
+
+    # there should be one folder
+    folder = os.path.join(tmp_dir, folders[0])
+    for f in os.listdir(folder):
         if f.startswith('IHME') and f.endswith('zip'):
-            shutil.move(os.path.join(source_dir, f), os.path.join(source_dir, 'DATA.zip'))
-            break
+            shutil.move(os.path.join(folder, f), source_dir)
     print('downloaded.')
 
 
